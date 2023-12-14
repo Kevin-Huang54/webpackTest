e71d519228eb9568f411f15d249d0101fedf4c90
Kevin Huang <huangjunjia@duoblock.cn>
2023-12-14 16:23:18 +0800

    配置react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+// import moment from "moment";
 function App() {
+  const log = async () => {
+    const moment = await import('moment');
+    alert(moment.default().format('MMMM Do YYYY, h:mm:ss a'));
+    // alert(moment().format('MMMM Do YYYY, h:mm:ss a'))
+  };
   return (
     <div>
       <h2>template_react_ts</h2>
+      <button onClick={log}>log import</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/index.tsx'), // 入口文件
+  entry: path.resolve(__dirname, './src/index.tsx'), // 入口文件
   output: {
     path: path.resolve(__dirname, 'dist'), // 输出文件夹的绝对路径,__dirname指当前文件所在的目录
-    filename: '[name].[chunkhash].js', // 输出的文件名
+    filename: '[name].[contenthash].js', // 输出的文件名
     clean: true, // build前删除原有文件夹
     publicPath: '/' // 打包后文件的公共前缀路径
   },
@@ -25,10 +25,12 @@ module.exports = {
     }]
   },
   plugins: [
-    // 打包html文件，同时生成的html中会引用main.js
-    new HtmlWebpackPlugin({ template: '../public/index.html' })
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: true // 自动注入静态资源
+    })
   ],
   resolve: {
-    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+    extensions: ['.js', '.tsx', '.ts'],
   }
 }
